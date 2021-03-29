3d192d4a3d33470d8e78bf3558acb4943785cf97
VasiliySherstnev <root@kola-nn.com>
2021-03-29 12:35:34 +0300

    init: start basicjs task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) != '[object Date]'){
+    throw new Error();
+  }
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  const month = date.getMonth();
+  if (month > 1 && month < 5) {
+    return 'spring';
+  } 
+  if (month > 4 && month < 8) {
+    return 'summer';
+  } 
+  if (month >7 && month < 11) {
+    return 'fall';
+  } 
+  return 'winter'
 };
