ec4b03701b4a7e94a8768ee22210a9ca5e8d610f
VasiliySherstnev <root@kola-nn.com>
2021-03-29 13:36:47 +0300

    fix: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if(!array) {
+    throw new Error('Error');
+  }
+
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+
+    switch (array[i]) {
+      case `--discard-next`:
+        i++;
+        break; 
+      case `--discard-prev`:
+        (newArray.length && array[i - 2] != `--discard-next`) ? newArray.pop() : newArray;
+        break;
+      case `--double-next`:
+        (i < array.length - 1) ? newArray.push(array[i+1]) : newArray;
+        break;
+      case `--double-prev`: (i > 0 && array[i - 2] != `--discard-next`) ? newArray.push(array[i-1]) : newArray;
+        break;
+      default: newArray.push(array[i])
+    }
+  }
+  return newArray
 };
